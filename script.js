a061348c4e237b545b78bcf8b47f4f915d20cd4b
Muhammad Salman <muhammad.salman@keeptrucki.com>
2022-12-27 17:33:54 +0500

    Completed movie booking functionality

---
+++ script.js
@@ -0,0 +1,28 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const selectedMovie = document.getElementById('movie');
+
+let ticketPrice = +selectedMovie.value;
+
+function updateSelectedCount(){
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+  const selectedSeatsCount = selectedSeats.length;
+
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+document.addEventListener('change', (e) => {
+  ticketPrice = e.target.value;
+  updateSelectedCount();
+});
+
+document.addEventListener('click', (e) => {
+  if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
+    e.target.classList.toggle('selected');
+
+    updateSelectedCount();
+  }
+});
