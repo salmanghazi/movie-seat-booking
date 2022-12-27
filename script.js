003d3bb1165a38fabd1877d6092927c68af6a932
Muhammad Salman <muhammad.salman@keeptrucki.com>
2022-12-27 18:08:05 +0500

    Stored seat data in local storage

--- script.js
+++ script.js
@@ -6,16 +6,46 @@ const selectedMovie = document.getElementById('movie');
 
 let ticketPrice = +selectedMovie.value;
 
-function updateSelectedCount(){
+restoreLocalStorageData();
+
+function restoreLocalStorageData() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    });
+  }
+
+
+  const selectedMovieIndex = +localStorage.getItem('selectedMovieIndex');
+  if (selectedMovieIndex !== null) {
+    selectedMovie.selectedIndex = selectedMovieIndex;
+  }
+}
+
+function saveSelectedMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
+function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
   const selectedSeatsCount = selectedSeats.length;
 
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
+
+  const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
 }
 
 document.addEventListener('change', (e) => {
-  ticketPrice = e.target.value;
+  ticketPrice = +e.target.value;
+  saveSelectedMovieData(e.target.selectedIndex, ticketPrice);
+
   updateSelectedCount();
 });
 
@@ -26,3 +56,6 @@ document.addEventListener('click', (e) => {
     updateSelectedCount();
   }
 });
+
+// on page load 
+updateSelectedCount();
